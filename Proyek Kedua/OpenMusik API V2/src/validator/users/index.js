4cc22401f72b7076b39d4ea0d70e9b851b09cd0d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-04-21 17:08:37 +0700

    Fix

--- Proyek Kedua/OpenMusik API V2/src/validator/users/index.js
+++ Proyek Kedua/OpenMusik API V2/src/validator/users/index.js
@@ -2,7 +2,7 @@ const InvariantError = require('../../exceptions/InvariantError');
 const { UserPayloadSchema } = require('./schema');
 
 const UsersValidator = {
-    validatorUserPayload: (payload) => {
+    validateUserPayload: (payload) => {
         const validationResult = UserPayloadSchema.validate(payload);
 
         if (validationResult.error) {
