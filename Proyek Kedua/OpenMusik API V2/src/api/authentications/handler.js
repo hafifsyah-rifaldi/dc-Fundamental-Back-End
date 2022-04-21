71295ca86ceb203d71c15011577a08796c7f5340
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-04-21 08:31:27 +0700

    Initial Commit

---
+++ Proyek Kedua/OpenMusik API V2/src/api/authentications/handler.js
@@ -0,0 +1,5 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class AuthenticationsHandler {
+
+}
---
+++ Proyek Kedua/OpenMusik API V2/src/api/authentications/routes.js
@@ -0,0 +1,19 @@
+const routes = (handler) => [
+    {
+        method: "POST",
+        path: '/authentications',
+        handler: () => {},
+    },
+    {
+        method: "PUT",
+        path: '/authentications',
+        handler: () => {},
+    },
+    {
+        method: :"DELETE",
+        path: '/authentications',
+        handler: () => {},  
+    },
+];
+
+module.exports = routes;
