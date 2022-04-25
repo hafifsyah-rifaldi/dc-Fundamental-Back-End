15ca773ab291b1c4657cfab660a9bce9fb306ac2
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-04-25 14:40:53 +0700

    Added Validator Joi Schema

---
+++ notes-app-back-end/src/validator/exports/index.js
@@ -0,0 +1,12 @@
+const ExportNotesPayloadschema = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const ExportsValidator = {
+    validateExportNotesPayload: (payload) => {
+        const validationResult = ExportNotesPayloadschema.validate(payload);
+
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message);
+        }
+    },
+};
