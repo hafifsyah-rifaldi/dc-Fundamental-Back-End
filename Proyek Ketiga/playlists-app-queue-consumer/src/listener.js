527840c2fdcbfa3cfb975693d03dad2c54fe1ad0
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-04-28 01:50:45 +0700

    Fix

--- Proyek Ketiga/playlists-app-queue-consumer/src/listener.js
+++ Proyek Ketiga/playlists-app-queue-consumer/src/listener.js
@@ -10,17 +10,11 @@ class Listener {
       try {
         const { playlistId, targetEmail } = JSON.parse(message.content.toString());
         console.log('Playlist ID:', playlistId);
+        console.log('targetEmail:', targetEmail);
 
         const songsInPlaylist = await this._playlistsService.getSongOnPlaylist(playlistId);
         console.log('Song in Playlist', songsInPlaylist);
 
-        // const playlistDetail = {
-        //   playlist: {
-        //     id: songsInPlaylist.id,
-        //     name: playlist.name,
-        //     songsInPlaylist
-        //   }
-        // }
         const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(songsInPlaylist));
         console.log(result);
       } catch (error) {
