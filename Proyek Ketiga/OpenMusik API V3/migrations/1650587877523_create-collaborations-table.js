4f6f25eca0cc1c2e988410332b2588685cb639fe
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-04-29 22:48:56 +0700

    Fix

--- Proyek Ketiga/OpenMusik API V3/migrations/1650587877523_create-collaborations-table.js
+++ Proyek Ketiga/OpenMusik API V3/migrations/1650587877523_create-collaborations-table.js
@@ -25,7 +25,7 @@ exports.up = (pgm) => {
    
     // memberikan constraint foreign key pada kolom playlist_id dan user_id terhadap playlist.id dan users.id
     pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
-    pgm.addConstraint('collaborations', 'fk_collaborations.playlist_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
+    pgm.addConstraint('collaborations', 'fk_collaborations.user_id_users.id', 'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE');
   };
    
   exports.down = (pgm) => {
